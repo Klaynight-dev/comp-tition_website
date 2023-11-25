80c47e439ada1c6737a50355ef602a1ec8922a30
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2023-11-25 20:11:21 +0100

    Add files via upload

---
+++ fctvalider.js
@@ -0,0 +1,4 @@
+function fctvalider() {
+  let user_name = document.getElementsByName('user_name');
+  console.log(user_name);
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+  console.log('La page a bien été chargé !');
+})
+
+function fctvalider(){
+  let a = document.getElementById('name');
+  console.log(a);
+}
+
+
